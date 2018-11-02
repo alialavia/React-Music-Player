cbf236e8daed5cd7aa814386cd5bded4e14f9bec
Ali Alavi <alialavia@gmail.com>
2018-11-01 21:50:34 -0400

    Seeker functional #2

--- src/Seeker.js
+++ src/Seeker.js
@@ -2,21 +2,33 @@ import ReactPlayer from "react-player";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import styled from "styled-components";
-
 import React, { Component } from "react";
-
 export default class Seeker extends Component {
-	render() {
-		// TODO: Make sure it's really click and not only mouse down
-		return (			
-			<div onMouseDown={(e)=>{ console.log(e.clientX);}} style={{ display: 'flex', backgroundColor: 'grey', height: 5, width: '80%' }}>
-				<span style={{ backgroundColor: '#a4da35', width: '50%'}}/>
-			</div>
-		);
-	}
+
+    render() {
+        return (
+            <div
+                onClick={event => {                    
+                    let bounds = event.currentTarget.getBoundingClientRect();
+                    let x = event.clientX - bounds.left;
+                    this.props.onChange(x / bounds.width);
+                }}
+                style={{ width: "100%", height: 5, backgroundColor: "grey" }}
+            >
+                <span
+                    style={{
+                        position: "absolute",
+                        backgroundColor: "#a4da35",
+                        width: `${this.props.time}%`,
+                        height: 5
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 Seeker.propTypes = {
-	time: PropTypes.number,
-	onChange: PropTypes.func
+    time: PropTypes.number,
+    onChange: PropTypes.func
 };
