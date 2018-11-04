294b40d0043e34620e8cc69630e0e34209a479b3
Ali Alavi <alialavia@gmail.com>
2018-11-04 12:03:26 -0500

    Test bug fixed

--- src/Helpers.test.js
+++ src/Helpers.test.js
@@ -1,24 +1,35 @@
 import * as jsc from "jsverify";
 import { padInt, formatSeconds } from "./Helpers.js";
 
+function numOfDigits(n) {
+	if (n == 0) return 1;
+	if (n < 0) return numOfDigits(-n);
+	return Math.floor(Math.log10(n)) + 1;
+}
+
 describe("padInt test suite.", function() {
-	
-	jsc.property("Padding cannot be negative and should throw exception", jsc.uint16, jsc.uint16, (a, b) =>
-		jsc.throws(() => padInt(a, -b - 1))
+	jsc.property(
+		"Padding cannot be negative and should throw exception",
+		jsc.uint16,
+		jsc.uint16,
+		(a, b) => jsc.throws(() => padInt(a, -b - 1))
 	);
 
 	jsc.property(
 		"For negative numbers, the result's length should be 1 more than the padding, to account for the negative sign",
 		jsc.uint16,
 		jsc.uint16,
-		(a, b) => padInt(-a - 1, b).length === b + 1
+		(a, b) =>
+			padInt(-a - 1, b).length ===
+			(numOfDigits(-a - 1) > b ? numOfDigits(-a - 1) + 1 : b + 1)
 	);
 
 	jsc.property(
 		"For positive numbers, the result's length should be eqaul to the padding",
 		jsc.uint16,
 		jsc.uint16,
-		(a, b) => padInt(a, b).length === b
+		(a, b) =>
+			padInt(a, b).length === (numOfDigits(a) > b ? numOfDigits(a) : b)
 	);
 
 	jsc.property(
@@ -30,25 +41,32 @@ describe("padInt test suite.", function() {
 });
 
 describe("formatSeconds test suite.", function() {
-
 	jsc.property(
 		"length should always >= 5",
 		jsc.uint16,
 		a => formatSeconds(a).length >= 5
 	);
-	
-	jsc.property("seconds length === 2", 
-		jsc.uint16, 
-		a => formatSeconds(a).split(":")[1].length === 2);
-	
-	jsc.property("minutes length >= 2", 
-		jsc.uint16, 
-		a => formatSeconds(a).split(":")[0].length >= 2);
 
-	jsc.property("converting back to seconds should return the same result", 
-		jsc.uint16, 
+	jsc.property(
+		"seconds length === 2",
+		jsc.uint16,
+		a => formatSeconds(a).split(":")[1].length === 2
+	);
+
+	jsc.property(
+		"minutes length >= 2",
+		jsc.uint16,
+		a => formatSeconds(a).split(":")[0].length >= 2
+	);
+
+	jsc.property(
+		"converting back to seconds should return the same result",
+		jsc.uint16,
 		a => {
-			let x = formatSeconds(a).split(":").map(e => parseInt(e));
+			let x = formatSeconds(a)
+				.split(":")
+				.map(e => parseInt(e));
 			return a === x[0] * 60 + x[1];
-		});
+		}
+	);
 });
